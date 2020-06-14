2bae0d9f12b743116c8e04bf3e0c9605bfb5592d
Szymon Borecki <szymex73@gmail.com>
2020-06-14 22:36:54 +0200

    Add base files for the project, bump to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thmcli",
-    version="0.0.1",
+    version="0.1.0",
     author="Szymon Borecki",
     author_email="self@szymex.pw",
     description="TryHackMe CLI",
@@ -20,4 +20,17 @@ setuptools.setup(
         "Development Status :: 3 - Alpha"
     ],
     python_requires='>=3.8',
+    install_requires=[
+        "argh==0.26.2",
+        "thmapi==0.8.0",
+        "requests",
+        "chardet>=3.0.2,<4",
+        "idna>=2.5,<3",
+        "urllib3>=1.21.1,<1.26,!=1.25.0,!=1.25.1",
+        "certifi>=2017.4.17"
+
+    ],
+    entry_points={
+        'console_scripts': ['thm = thmcli.cli:main']
+    },
 )
